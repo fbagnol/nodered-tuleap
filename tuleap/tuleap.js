26d0440f96e658b209f04079c531d59d961d79ba
Frédéric Bagnol <fbagnol@tec6.fr>
2023-01-11 16:48:16 +0100

    passage de expert_query a query

--- tuleap/tuleap.js
+++ tuleap/tuleap.js
@@ -97,7 +97,7 @@ module.exports = function(RED) {
             var options = {
                 rejectUnauthorized: false,
                 strictSSL: false,
-                uri: decodeURIComponent(url + 'trackers/' + options.id + '/artifacts?values=all&limit=100&offset=0&expert_query=' + query + '&order=asc'),
+                uri: decodeURIComponent(url + 'trackers/' + options.id + '/artifacts?values=all&limit=100&offset=0&query=' + query + '&order=asc'),
                 method: 'GET',
                 json: true,
                 followAllRedirects: true,
