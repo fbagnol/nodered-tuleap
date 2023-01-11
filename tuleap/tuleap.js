f09f0bb2d6b2bd1819e4de772958e18e58d05e1b
Frédéric Bagnol <fbagnol@tec6.fr>
2023-01-11 16:29:34 +0100

    strictSSL false

--- tuleap/tuleap.js
+++ tuleap/tuleap.js
@@ -96,6 +96,7 @@ module.exports = function(RED) {
 
             var options = {
                 rejectUnauthorized: false,
+                strictSSL: false,
                 uri: decodeURIComponent(url + 'trackers/' + options.id + '/artifacts?values=all&limit=100&offset=0&expert_query=' + query + '&order=asc'),
                 method: 'GET',
                 json: true,
